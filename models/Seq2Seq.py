b7b33a7ce635408c17f334e16799962f2bbb2cb5
Krishna D N <krishnadn94@gmail.com>
2020-06-07 10:09:06 +0530

    Data processing scripts for TIMIT

--- models/Seq2Seq.py
+++ models/Seq2Seq.py
@@ -16,9 +16,9 @@ class Seq2seq(nn.Module):
     def forward(self, input_variable, input_lengths=None, target_variable=None,
                 teacher_forcing_ratio=0):
         encoder_outputs, encoder_hidden = self.encoder(input_variable, input_lengths)
-        result = self.decoder(inputs=target_variable,
+        decoder_ouputs,seq_symbols = self.decoder(inputs=target_variable,
                               encoder_hidden=encoder_hidden,
                               encoder_outputs=encoder_outputs,
                               function=self.decode_function,
                               teacher_forcing_ratio=teacher_forcing_ratio)
-        return result
+        return decoder_ouputs,seq_symbols
